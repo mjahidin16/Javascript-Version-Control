1c6790238c26098708c75a64e20f579f22914ae5
muhamad jahidin <133470779+mjahidin16@users.noreply.github.com>
2023-11-02 22:10:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Membuat array dengan 100 nilai acak antara 1 dan 50
 function createRandomArray(length) {
   const randomArray = [];
   for (let i = 0; i < length; i++) {
@@ -7,7 +6,6 @@ function createRandomArray(length) {
   return randomArray;
 }
 
-// Memisahkan array menjadi array genap dan ganjil
 function splitArrayByParity(arr) {
   const evenArray = [];
   const oddArray = [];
@@ -21,8 +19,10 @@ function splitArrayByParity(arr) {
   return [evenArray, oddArray];
 }
 
-// Menghitung Min
 function findMin(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
   let min = arr[0];
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] < min) {
@@ -32,8 +32,10 @@ function findMin(arr) {
   return min;
 }
 
-// Menghitung Max
 function findMax(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
   let max = arr[0];
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] > max) {
@@ -43,7 +45,6 @@ function findMax(arr) {
   return max;
 }
 
-// Menghitung Total
 function findTotal(arr) {
   let total = 0;
   for (let i = 0; i < arr.length; i++) {
@@ -52,7 +53,6 @@ function findTotal(arr) {
   return total;
 }
 
-// Menghitung Rata-rata
 function findAverage(arr) {
   if (arr.length === 0) {
     return 0;
@@ -61,77 +61,65 @@ function findAverage(arr) {
   return total / arr.length;
 }
 
+function compareValues(array1, array2) {
+  const min1 = findMin(array1);
+  const min2 = findMin(array2);
+  const max1 = findMax(array1);
+  const max2 = findMax(array2);
+  const total1 = findTotal(array1);
+  const total2 = findTotal(array2);
+  const average1 = findAverage(array1);
+  const average2 = findAverage(array2);
+
+  console.log("Min, Max, Total, dan Rata-rata pada array genap:");
+  console.log("Min Genap:", min1);
+  console.log("Max Genap:", max1);
+  console.log("Total Genap:", total1);
+  console.log("Rata-rata Genap:", average1);
+
+  console.log("Min, Max, Total, dan Rata-rata pada array ganjil:");
+  console.log("Min Ganjil:", min2);
+  console.log("Max Ganjil:", max2);
+  console.log("Total Ganjil:", total2);
+  console.log("Rata-rata Ganjil:", average2);
+
+  console.log("Perbandingan nilai Min, Max, Total, dan Rata-rata:");
+
+  if (min1 > min2) {
+    console.log("Min lebih besar pada: array genap");
+  } else if (min1 < min2) {
+    console.log("Min lebih besar pada: array ganjil");
+  } else {
+    console.log("Min sama pada kedua array");
+  }
+
+  if (max1 > max2) {
+    console.log("Max lebih besar pada: array genap");
+  } else if (max1 < max2) {
+    console.log("Max lebih besar pada: array ganjil");
+  } else {
+    console.log("Max sama pada kedua array");
+  }
+
+  if (total1 === total2) {
+    console.log("Total sama pada kedua array");
+  } else {
+    console.log("Total berbeda pada kedua array");
+  }
+
+  if (average1 > average2) {
+    console.log("Rata-rata lebih besar pada: array genap");
+  } else if (average1 < average2) {
+    console.log("Rata-rata lebih besar pada: array ganjil");
+  } else {
+    console.log("Rata-rata sama pada kedua array");
+  }
+}
+
 // Main program
 const randomArray = createRandomArray(100);
 const [evenArray, oddArray] = splitArrayByParity(randomArray);
-
-const minEven = findMin(evenArray);
-const maxEven = findMax(evenArray);
-const totalEven = findTotal(evenArray);
-const averageEven = findAverage(evenArray);
-
-const minOdd = findMin(oddArray);
-const maxOdd = findMax(oddArray);
-const totalOdd = findTotal(oddArray);
-const averageOdd = findAverage(oddArray);
-
+compareValues(evenArray, oddArray);
 console.log("Array dengan jumlah index 100:", randomArray);
 console.log("Array genap dengan jumlah index 50:", evenArray);
 console.log("Array ganjil dengan jumlah index 50:", oddArray);
-
-console.log("Min, Max, Total, dan Rata-rata pada array genap:");
-console.log("Min Genap:", minEven);
-console.log("Max Genap:", maxEven);
-console.log("Total Genap:", totalEven);
-console.log("Rata-rata Genap:", averageEven);
-
-console.log("Min, Max, Total, dan Rata-rata pada array ganjil:");
-console.log("Min Ganjil:", minOdd);
-console.log("Max Ganjil:", maxOdd);
-console.log("Total Ganjil:", totalOdd);
-console.log("Rata-rata Ganjil:", averageOdd);
-
-console.log("Perbandingan nilai Min, Max, Total, dan Rata-rata:");
-
-// Percabangan
-if (minEven > minOdd) {
-  console.log("Min lebih besar pada: array genap");
-} else if (minEven < minOdd) {
-  console.log("Min lebih besar pada: array ganjil");
-} else {
-  console.log("Min sama pada kedua array");
-}
-
-if (maxEven > maxOdd) {
-  console.log("Max lebih besar pada: array genap");
-} else if (maxEven < maxOdd) {
-  console.log("Max lebih besar pada: array ganjil");
-} else {
-  console.log("Max sama pada kedua array");
-}
-
-if (totalEven === totalOdd) {
-  console.log("Total sama pada kedua array");
-} else {
-  console.log("Total berbeda pada kedua array");
-}
-
-if (averageEven > averageOdd) {
-  console.log("Rata-rata lebih besar pada: array genap");
-} else if (averageEven < averageOdd) {
-  console.log("Rata-rata lebih besar pada: array ganjil");
-} else {
-  console.log("Rata-rata sama pada kedua array");
-}
-
-// Variabel
-// Dalam kode tersebut, kita menggunakan variabel seperti randomArray, evenArray, oddArray, minEven, maxEven, totalEven, averageEven, minOdd, maxOdd, totalOdd, dan averageOdd
-// Tipe Data:
-// Dalam kode tersebut, kita menggunakan tipe data number, numerik, string.
-// Operator:
-// operator matematika seperti +, -, *, /, % Operator perbandingan seperti >, <, ===
-// Percabangan:
-// if, else if, dan else.
-// Perulangan: for dalam fungsi createRandomArray(length).
-// Array: randomArray, evenArray, dan oddArray.
-// Fungsi:createRandomArray(length), splitArrayByParity(arr), findMin(arr), findMax(arr), findTotal(arr), dan findAverage(arr).
